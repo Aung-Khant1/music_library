924a74411d7fda3aca651b9404f9a0a782ce5a69
WannaMinPaing <paingwanna@gmail.com>
2020-11-13 22:49:46 +0630

    Local override error prepare

--- Music_Library/routes/web.php
+++ Music_Library/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//wanna route start
+  Route::resource('singer', 'SingerController'); // 7 (get, post, put, delete)
+  Route::resource('song', 'SongController'); // 7 
+  //Route::resource('subcategory', 'SubcategoryController'); // 7 
+  //Route::resource('item', 'ItemController'); // 7 
+//wanna route end
