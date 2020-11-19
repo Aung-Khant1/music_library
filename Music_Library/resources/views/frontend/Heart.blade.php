c61e65c82d433e04c9d52e5208502fd3bfe5b988
Woody <1122aungkhant@gmail.com>
2020-11-19 19:14:04 +0630

    nothing big change

--- Music_Library/resources/views/frontend/Heart.blade.php
+++ Music_Library/resources/views/frontend/Heart.blade.php
@@ -35,9 +35,7 @@
                                     <li><a href="{{route('contact')}}">Contact</a></li>
                                      <li><a href="{{route('Heart')}}">Favourite</a></li>
                                     <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
+                                        
 
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                             <a class="dropdown-item" href="{{ route('logout') }}"
@@ -50,6 +48,10 @@
                                                 @csrf
                                             </form>
                                         </div>
+
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
                                     </li>
                                 </ul>
 
--- Music_Library/resources/views/frontend/mainpage.blade.php
+++ Music_Library/resources/views/frontend/mainpage.blade.php
@@ -43,9 +43,7 @@
                                     <li><a href="{{route('contact')}}">Contact</a></li>
                                     <li><a href="{{route('Heart')}}">Favourite</a></li>
                                     <li class="nav-item dropdown">
-                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                            {{ Auth::user()->name }}
-                                        </a>
+                                        
 
                                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                             <a class="dropdown-item" href="{{ route('logout') }}"
@@ -58,6 +56,9 @@
                                                 @csrf
                                             </form>
                                         </div>
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
                                     </li>
                                 </ul>
 
